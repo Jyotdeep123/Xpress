0a7fe7592fef95b573e1d5034af1d1b1d9aaef6f
jyotdeep <jyotdeep@humber.ca>
2023-11-28 14:45:03 -0500

    url updated and procfile added

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/newapp', {
+mongoose.connect('mongodb+srv://MrJyotdeep:admin123@cluster0.3de5x92.mongodb.net', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log('MongoDb Connected'))
